8dd2de2536475aae51bd7cdce368b5c4b173412e
kevin cifuentes <kevincifuentes@MacBook-de-kevin.local>
2013-02-28 17:58:55 -0500

    First commit
    Signed-off-by: kevin cifuentes <kevincifuentes@MacBook-de-kevin.local>

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Prueba::Application.config.secret_token = '338b061a9b06c82a9c72bd30b5af36cc41ddfe1d7df55024854a6d6220b99e5e336c0f54eba150d9070a915dd2335e688bd00a0d727db19f1d65be1e3113c13f'
